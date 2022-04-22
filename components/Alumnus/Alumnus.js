af30c9637d3d7df77b522b8e78f2dff310924950
Sam Wagner <sfraserwagner@gmail.com>
2022-04-21 17:12:25 -0700

    Make Alumni names H6

--- components/Alumnus/Alumnus.js
+++ components/Alumnus/Alumnus.js
@@ -4,7 +4,7 @@ import styles from './Alumnus.module.scss';
 const Alumnus = ({person}) => {
     return (
         <div className={`${styles.Alumnus} alumni-grid-item`}>
-            {(person.firstName || person.lastName) && <h5>{person.firstName} {person.lastName}</h5>}
+            {(person.firstName || person.lastName) && <h6>{person.firstName} {person.lastName}</h6>}
             {person.currentPosition && <p className="body5">{person.currentPosition}</p>}
         </div>
     );
